4f0bac75393574d47220e7185024eb02499468d8
Satya <satya@Satyanarayanas-MacBook-Air.local>
2017-10-16 14:15:28 -0600

    setup complte

--- FirebaseChats/ViewController.swift
+++ FirebaseChats/ViewController.swift
@@ -8,17 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.backgroundColor = UIColor.red
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
