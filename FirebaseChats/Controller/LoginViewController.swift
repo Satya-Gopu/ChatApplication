1b3ef962060eca9528ebb5d5f26297579f340af1
satya <sgopu@unm.edu>
2017-10-16 21:02:50 -0600

    setup form container view

--- FirebaseChats/Controller/LoginViewController.swift
+++ FirebaseChats/Controller/LoginViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let inputContainerView : UIView = {
+        let inputContainerView = UIView()
+        inputContainerView.backgroundColor = UIColor.white
+        inputContainerView.layer.cornerRadius = 5
+        inputContainerView.translatesAutoresizingMaskIntoConstraints = false
+        return inputContainerView
+        }()
+    
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return UIStatusBarStyle.lightContent
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.view.backgroundColor = UIColor(red:61, green: 91, blue: 151)
+        //create container view for the input form
+        self.view.addSubview(inputContainerView)
+        setupContainerView()
+    }
+    
+    func setupContainerView(){
+        //set the constraints for the container view: x, y, width, height
+        NSLayoutConstraint.activate([
+            inputContainerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            inputContainerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            inputContainerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, constant: -24),
+            inputContainerView.heightAnchor.constraint(equalToConstant: 150)])
+        
     }
 
 }
